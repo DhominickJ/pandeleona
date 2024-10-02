a43114462d7363eb69b7c132e3be54101a18f3b8
myangft <marianefaith.torreverde@wvsu.edu.ph>
2024-10-03 02:19:58 +0800

    Updated Files

--- src/preload.js
+++ src/preload.js
@@ -1,6 +1,6 @@
-import '@phosphor-icons/web/bold'
-import '@phosphor-icons/web/fill'
-import '@fontsource/poppins'
+// import '@phosphor-icons/web/bold'
+// import '@phosphor-icons/web/fill'
+//import '@fontsource/poppins'
 
 import './style/main.scss'
 import './style/home.scss'
